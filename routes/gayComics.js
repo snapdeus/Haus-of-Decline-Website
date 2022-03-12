3bd15d3be10246b7c5b36f403da588a1f518172e
snapdeus <snapdeus@gmail.com>
2022-03-12 09:26:10 -0500

    changed uploads dir in multer, fixed edit

--- routes/gayComics.js
+++ routes/gayComics.js
@@ -10,7 +10,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads')
+        cb(null, './uploads/GayComics')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
