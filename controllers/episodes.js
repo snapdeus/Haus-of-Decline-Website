1ef209bd83c677dc85b8dc18b8e5c1c6595ae352
snapdeus <snapdeus@gmail.com>
2022-05-10 09:09:39 -0400

    testing error

--- controllers/episodes.js
+++ controllers/episodes.js
@@ -40,7 +40,7 @@ const config = { headers: { 'x-api-key': apiKey } }
 module.exports.index = async (req, res) => {
     const pageNumber = req.params.page;
     if (!pageNumber) {
-        pageNumber = 1;
+        pageNumber = "1";
         res.redirect("episodes/1")
     }
     const getShows = async () => {
