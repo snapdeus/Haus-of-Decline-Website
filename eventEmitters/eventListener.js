585d2859eb98be5411d438abb6cc277fcb306856
snapdeus <snapdeus@gmail.com>
2022-05-10 23:03:22 -0400

    bug testing

--- eventEmitters/eventListener.js
+++ eventEmitters/eventListener.js
@@ -35,6 +35,7 @@ client.on('ready', () => {
         const content = unformattedContent.replace(/<[^>]*>?/gm, '')
         const title = response.data.attributes.title;
         const url = response.data.attributes.url
+        const file = new Discord.MessageAttachment('../gifs/resources/oink.png');
 
         if (title.includes('Daily')) {
             const embedMsg = new Discord.MessageEmbed()
@@ -42,41 +43,40 @@ client.on('ready', () => {
                 .setTitle(`New Comic on Patreon!: ${ title }`)
                 .setDescription(`${ content }`)
                 .setURL(`https://www.patreon.com${ url }`)
-                .setThumbnail('https://i.imgur.com/YsPMVOH.png')
-            client.channels.cache.get(process.env.TESTGENERAL_CHANNEL).send({ embeds: [embedMsg] })
+                .setThumbnail('attachment://oink.png')
+            client.channels.cache.get(process.env.TESTGENERAL_CHANNEL).send({ embeds: [embedMsg], files: [file] })
         } else if (title.includes('BE')) {
             const embedMsg = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle(`New Bonus Episode! ${ title }`)
                 .setDescription(`${ content }`)
                 .setURL(`https://www.patreon.com${ url }`)
-                .setThumbnail('https://i.imgur.com/YsPMVOH.png')
-            client.channels.cache.get(process.env.TESTGENERAL_CHANNEL).send({ embeds: [embedMsg] })
+                .setThumbnail('attachment://oink.png')
+            client.channels.cache.get(process.env.TESTGENERAL_CHANNEL).send({ embeds: [embedMsg], files: [file] })
         } else if (title.includes('Behind')) {
             const embedMsg = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle(`New Behind the Scenes post!: ${ title }`)
                 .setDescription(`${ content }`)
                 .setURL(`https://www.patreon.com${ url }`)
-                .setThumbnail('https://i.imgur.com/YsPMVOH.png')
-            client.channels.cache.get(process.env.TESTGENERAL_CHANNEL).send({ embeds: [embedMsg] })
+                .setThumbnail('attachment://oink.png')
+            client.channels.cache.get(process.env.TESTGENERAL_CHANNEL).send({ embeds: [embedMsg], files: [file] })
         } else if (title.includes('Commission')) {
             const embedMsg = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle(`New Commissioned Comic!: ${ title }`)
                 .setDescription(`${ content }`)
                 .setURL(`https://www.patreon.com${ url }`)
-                .setThumbnail('https://i.imgur.com/YsPMVOH.png')
-            client.channels.cache.get(process.env.TESTCOMMISSION_CHANNEL).send({ embeds: [embedMsg] })
+                .setThumbnail('attachment://oink.png')
+            client.channels.cache.get(process.env.TESTCOMMISSION_CHANNEL).send({ embeds: [embedMsg], files: [file] })
         } else {
-
             const embedMsg = new Discord.MessageEmbed()
                 .setColor('#0099ff')
                 .setTitle(`New Post on Patreon: ${ title }`)
                 .setDescription(`${ content }`)
                 .setURL(`https://www.patreon.com${ url }`)
-                .setThumbnail('https://i.imgur.com/YsPMVOH.png')
-            client.channels.cache.get(process.env.TESTGENERAL_CHANNEL).send({ embeds: [embedMsg] })
+                .setThumbnail('attachment://oink.png')
+            client.channels.cache.get(process.env.TESTGENERAL_CHANNEL).send({ embeds: [embedMsg], files: [file] })
         }
     })
 })
