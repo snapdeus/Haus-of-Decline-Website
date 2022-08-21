36383297b48393c81a892ba2ac12c739215692e4
snapdeus <snapdeus@gmail.com>
2022-08-21 08:49:12 -0400

    temp fix, remove scroller.js

--- public/js/scroller.js
+++ public/js/scroller.js
@@ -1,11 +1,13 @@
-$(function () {
-    $(document).scroll(function () {
-        var $nav = $("#topkeep");
-        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-        var $drop = $("#navbarSupportedContent");
-        $drop.removeClass('show', $(this).scrollTop() > $nav.height());
-    })
-})
+// $(function () {
+
+//     $(document).scroll(function () {
+//         var $nav = $("#topkeep");
+//         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+//         var $drop = $("#navbarSupportedContent");
+//         $drop.removeClass('show', $(this).scrollTop() > $nav.height());
+//     })
+// })
+
 $(".navbar-toggler").click(function () {
     $(".navbar").addClass("activated");
 });
