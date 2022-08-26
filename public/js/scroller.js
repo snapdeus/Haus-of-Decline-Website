2056fe72f225a3aadf96a9d6ad990aaa7463f736
snapdeus <snapdeus@gmail.com>
2022-08-26 17:18:34 -0400

    add back in black bg to navbar when scrolled

--- public/js/scroller.js
+++ public/js/scroller.js
@@ -1,12 +1,12 @@
-// $(function () {
+$(function () {
 
-//     $(document).scroll(function () {
-//         var $nav = $("#topkeep");
-//         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-//         var $drop = $("#navbarSupportedContent");
-//         $drop.removeClass('show', $(this).scrollTop() > $nav.height());
-//     })
-// })
+    $(document).scroll(function () {
+        var $nav = $("#topkeep");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        // var $drop = $("#navbarSupportedContent");
+        // $drop.removeClass('show', $(this).scrollTop() > $nav.height());
+    })
+})
 
 $(".navbar-toggler").click(function () {
     $(".navbar").addClass("activated");
