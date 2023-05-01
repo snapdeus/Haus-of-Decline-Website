4141769c52bdeee5599c6d430da63f833685d227
snapdeus <snapdeus@gmail.com>
2023-05-01 10:40:57 -0400

    revert captcha to prod

--- index.js
+++ index.js
@@ -170,8 +170,8 @@ const sessionConfig = {
     saveUninitialized: true,
     proxy: true,
     cookie: {
-        // httpOnly: true,
-        // secure: true,
+        httpOnly: true,
+        secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
